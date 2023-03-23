20a212c3b7b81339e37797660009810bea28355b
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-03-23 16:20:06 +0900

    feat : init project

---
+++ FinalSendoff/Source/FinalSendoff/FinalSendoff.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinalSendoff.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalSendoff, "FinalSendoff" );
+ 
---
+++ FinalSendoff/Source/FinalSendoff/FinalSendoffGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FinalSendoffGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFinalSendoffGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFinalSendoffGameMode();
+};
+
+
+
